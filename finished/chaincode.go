b83f63ee9b1598a368afec3386e950dbae0d25ec
mdonthi <mdonthi@us.ibm.com>
2016-10-24 11:30:25 -0500

    asset code added

--- finished/chaincode.go
+++ finished/chaincode.go
@@ -1,6 +1,6 @@
 package main
 
-import{
+import(
 	"errors"
 	"fmt"
 	"strconv"
@@ -8,7 +8,7 @@ import{
 	"strings"
 
 	"github.com/openblockchain/obc-peer/openchain/chaincode/shim"
-}
+)
 
 type Chaincode struct {
 }
